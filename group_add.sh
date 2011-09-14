53b6781a6458ed014cb055e7948a030ecba5dbab
Dmitry Pavlov <zeldigas@gmail.com>
2011-09-14 18:16:52 +0400

    added hook script for group create/delete actions

---
+++ group_add.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+GROUP_PATTERN="^class[1-9]+[:alpa:]*"
+
+CLASS_GROUPS_DIR=/srv/user_dirs/groups_dir
+
+group_name=$USERADMIN_GROUP
+
+function create_group_dir(){
+    if echo $group_name | grep -qE $GROUP_PATTERN - ;
+    then
+	mkdir "$CLASS_GROUPS_DIR/$group_name"
+    fi
+}
+
+function remove_group_dir(){
+    rm -f "$CLASS_GROUPS_DIR/$group_name/"*
+    rmdir $CLASS_GROUPS_DIR/$group_name"
+}
+
+if [[ "$USERADMIN_ACTION" == "CREATE_GROUP" ]];
+then
+    create_group_dir
+elif [[ "$USERADMIN_ACTION" == "DELETE_GROUP" ]]
+    remove_group_dir
+fi
