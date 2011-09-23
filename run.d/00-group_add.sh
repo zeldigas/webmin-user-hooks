a62d6988fcce6e22b402050bd14ed164e19023ed
Dmitry Pavlov <zeldigas@gmail.com>
2011-09-23 22:27:08 +0400

    added set rights and own group for class dir

--- run.d/00-group_add.sh
+++ run.d/00-group_add.sh
@@ -4,13 +4,17 @@
 GROUP_PATTERN="^class[1-9]+[:alpa:]*"
 
 CLASS_GROUPS_DIR=/srv/user_dirs/groups_dir
+DIR_GROUP=class_admins
 
 group_name=$USERADMIN_GROUP
 
 function create_group_dir(){
     if echo $group_name | grep -qE $GROUP_PATTERN - ;
     then
-	mkdir "$CLASS_GROUPS_DIR/$group_name"
+	new_dir_path="$CLASS_GROUPS_DIR/$group_name"
+	mkdir "$new_dir_path"
+	chgrp $DIR_GROUP "$new_dir_path"
+	chmod 775 "$new_dir_path"
     fi
 }
 
